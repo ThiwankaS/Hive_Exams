38f90e29664d67dbe2335d047d35a56d2526b149
Thiwanka Somachandra <thivankas@outlook.com>
2025-01-16 06:01:31 +0200

    solution - comment out rest

--- Rank02/Level02/reverse_bits/reverse_bits.c
+++ Rank02/Level02/reverse_bits/reverse_bits.c
@@ -1,16 +1,16 @@
-#include <unistd.h>
+// #include <unistd.h>
 
-void print_bits(unsigned char octet)
-{
-	int i = 8;
-	unsigned char 	bit;
+// void print_bits(unsigned char octet)
+// {
+// 	int i = 8;
+// 	unsigned char 	bit;
 
-	while (i--)
-	{
-		bit = (octet >> i & 1) + '0';
-		write(1, &bit, 1);
-	}
-}
+// 	while (i--)
+// 	{
+// 		bit = (octet >> i & 1) + '0';
+// 		write(1, &bit, 1);
+// 	}
+// }
 unsigned char	reverse_bits(unsigned char octet)
 {
 	unsigned char result = 0x0;
@@ -25,15 +25,15 @@ unsigned char	reverse_bits(unsigned char octet)
 	return (result);
 }
 
-int main(void)
-{
-	unsigned char ch = 0x02;
-	write(1,"Before : ",10);
-	print_bits(ch);
-	write(1,"\n",1);
-	write(1,"After  : ",10);
-	print_bits(reverse_bits(ch));
-	write(1,"\n",1);
+// int main(void)
+// {
+// 	unsigned char ch = 0x02;
+// 	write(1,"Before : ",10);
+// 	print_bits(ch);
+// 	write(1,"\n",1);
+// 	write(1,"After  : ",10);
+// 	print_bits(reverse_bits(ch));
+// 	write(1,"\n",1);
 
-	return (0);
-}
+// 	return (0);
+// }
